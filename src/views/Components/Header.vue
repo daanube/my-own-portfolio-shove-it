ff8e16f097bacb85e194e8ed6f81541d7b490a86
David Nuñez <32456248+daanube@users.noreply.github.com>
2025-09-22 14:06:15 -0400

    Componentizando la cabecera
    Componentizando la cabecera

---
+++ src/views/Components/Header.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+    const props = defineProps({
+        headText: {
+            type: String,
+            default: "",
+            required: true,
+        },
+        imageUrl: {
+            type: String,
+            default: "",
+            required: true,
+        }
+    });
+</script>
+
+<!-- ////////////////////////////////////////////////////////////////////////////////////// -->
+
+<template>
+    <div class="row">
+        <div class="col-12">
+            <div id="HeadImg">
+                <div class="Head">
+                    {{ props.headText }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<!-- ////////////////////////////////////////////////////////////////////////////////////// -->
+
+<style lang="css" scoped>
+    .Head {
+        margin: 0 auto;
+        padding: 3rem 0;
+        width: 100%;
+        color: #fff;
+        text-align: center;
+        font-size: 140px;
+        font-weight: bolder;
+    }
+    #HeadImg {
+        align-content: center;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 45%;
+        background-image: v-bind("props.imageUrl");
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        filter: saturate(0.5);
+        line-height: 8rem;
+    }
+</style>
--- src/views/Welcome/WelcomeView.vue
+++ src/views/Welcome/WelcomeView.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
     import SideBarLayout from '../../layouts/SideBarLayout.vue';
+    import Header from '../Components/Header.vue';
+
+    const welceomeTitle = "DAVID NUÑEZ DESARROLLADOR";
+    const bgUrl = "/src/assets/images/bgs/michelle-ding-unsplash.jpg";
 </script>
 
 <!-- //////////////////////////////////////////////////////////////////////// -->
@@ -9,12 +13,7 @@
     <SideBarLayout>
         <div class="row" id="welcome">
             <div class="col-12 slotArea">
-                <div id="welcomeHeadImg">
-                    <div class="welcomeHead">
-                        DAVID NUÑEZ
-                        DESARROLLADOR
-                    </div>
-                </div>
+                <Header :head-text='welceomeTitle' :image-url='`url(${bgUrl})`' ></Header>
                 <article id="welcomeArticle">
                     <h3 id="welcomeArticleTitle">Hola, wapo</h3>
                 </article>
@@ -27,29 +26,6 @@
 <!-- //////////////////////////////////////////////////////////////////////// -->
 
 <style lang="css">
-    .welcomeHead {
-        margin: 0 auto;
-        padding: 0;
-        width: 100%;
-        color: #fff;
-        text-align: center;
-        font-size: 140px;
-        font-weight: bolder;
-    }
-    #welcomeHeadImg {
-        /* display: flex; */
-        align-content: center;
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: 45%;
-        background-image: url("/src/assets/images/bgs/michelle-ding-unsplash.jpg");
-        background-position: center;
-        background-repeat: no-repeat;
-        background-size: cover;
-        filter: saturate(0.5);
-        line-height: 8rem;
-    }
     #welcomeArticle {
         border-radius: 5px;
         background-color: aliceblue;
