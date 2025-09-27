8c188b050a4b7dcc8d3350b5e12e96951855e087
David Nuñez <32456248+daanube@users.noreply.github.com>
2025-09-27 17:13:42 -0400

    Corrigiendo rutas de imagenes para el deploy
    Corrigiendo rutas de imagenes para el deploy

--- src/views/Welcome/WelcomeView.vue
+++ src/views/Welcome/WelcomeView.vue
@@ -6,7 +6,7 @@
     import TechStackChipList from '../Components/TechStackChipList.vue';
 
     const welcomeTitle = "DAVID NUÑEZ DESARROLLADOR";
-    const bgUrl = "/src/assets/images/bgs/michelle-ding-unsplash.jpg";
+    const bgUrl = "/images/bgs/michelle-ding-unsplash.jpg";
     const articleText = "Soy un Desarrollador Web con más de 5 años de experiencia en proyectos de lotería, retail, atención médica y delivery, desempeñándome en el área de Front-End, y como apoyo en tareas varias a los diferentes equipos de Back-End. Esto me ha dado las herramientas tanto técnicas como de análisis para tener una perspectiva amplia y poder abordar proyectos y crear soluciones desde varios enfoques. Cuento con experiencia en Desarrollo Web con JavaScript, TypeScript, Angular y Vue, y en escritorio con C# y WPF. Creación de reportes con Crystal Reports.";  
 </script>
 
