2cca22b590542889b6d3bc055cd57d5ace6510ba
daanube <daanube@gmail.com>
2025-10-07 19:28:17 -0400

    Creacion de componente windowShape
    Creacion de componente windowShape

---
+++ src/components/WindowShape.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+
+</script>
+
+<!-- //////////////////////////////////////////////////////////////////////// -->
+
+<template>
+    <main id="windowshape">
+        <section id="titleBar">
+            <i>x</i>
+            <i>[]</i>
+            <i>_</i>
+        </section>
+        <section id="frame">
+            <slot />
+        </section>
+    </main>
+</template>
+
+<!-- //////////////////////////////////////////////////////////////////////// -->
+
+<style lang="css" scoped>
+    #windowshape {
+        margin: 3rem;
+        border: solid 5px #fff;
+        display: grid;
+        grid-template-areas: "titleBar" "frame";
+        grid-template-rows: 1fr;
+        grid-template-columns: 1fr;
+        background-color: var(--darkColor-2);
+
+    }
+    #titleBar {
+        display: flex;
+        flex-direction: row-reverse;
+        grid-area: titleBar;
+        height: 2rem;
+    }
+    #frame {
+        grid-area: frame;
+    }
+    i {
+        font-size: 0.7rem;
+        font-weight: 600;
+        border: 1px solid #fff;
+        color: #fff;
+        padding: 10px;
+    }
+</style>
--- src/views/Projects/DeliveryDemoView.vue
+++ src/views/Projects/DeliveryDemoView.vue
@@ -2,6 +2,7 @@
     import SideBarLayout from '../../layouts/SideBarLayout.vue';
     import Article from '../../components/ArticleComponent.vue';
     import MainTitle from '../../components/MainTitleComponent.vue';
+import WindowShape from '../../components/WindowShape.vue';
     
     const title = "Venta y reparto de productos";
     const bgUrl = "/images/bgs/keyboard-mechanical-keyboard-3196030_1280-min.jpg";
@@ -16,6 +17,11 @@
         <div class="row mainTitle" id="delivery">
             <div class="col-12 slotArea">
                 <MainTitle :mainTitleText='title' :image-url='`url(${bgUrl})`' />
+                
+                <WindowShape>
+                    <MainTitle :mainTitleText='title' :image-url='`url(${bgUrl})`' />
+                </WindowShape>
+
                 <Article :articleText="articleText" />
             </div>
         </div>
