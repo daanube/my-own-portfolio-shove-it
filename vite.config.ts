a7469a1ff6997c649ff73f72ee68b08330700f48
daanube <daanube@gmail.com>
2025-10-09 19:04:37 -0400

    instalando module federation plugin para hostear una  app en vue3
    instalando module federation plugin para hostear una  app en vue3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import federation from "@originjs/vite-plugin-federation";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    federation({
+        name: 'host-app',
+        remotes: {
+            // cambiar por la url del proyecto que se hosteará
+            remote_app: "http://localhost:5001/assets/remoteEntry.js",
+        },
+        shared: ['vue']
+    })
+  ],
 })
